73ed91e9fef118032d007741a10e0c8bc920b440
is716652 <is716652@users.noreply.github.com>
2025-08-24 12:59:54 +0800

    第二次更新

--- 文案/PPT/enhanced-demo-combined.js
+++
@@ -1,4 +0,0 @@
-// 加载所有部分
-document.write('<script src="enhanced-demo-part1.js"></script>');
-document.write('<script src="enhanced-demo-part2.js"></script>');
-document.write('<script src="enhanced-demo-part3.js"></script>');
